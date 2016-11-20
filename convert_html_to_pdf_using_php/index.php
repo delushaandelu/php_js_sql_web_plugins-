88ad21ed1c426b7c121a69e49fc2ef1aaddc3b49
Delushaan <delushaan@outlook.com>
2016-11-20 08:45:23 +0530

    convert_html_to_pdf_using_php

---
+++ convert_html_to_pdf_using_php/index.php
@@ -0,0 +1,24 @@
+<?php
+// include autoloader
+require_once 'dompdf/autoload.inc.php';
+
+// reference the Dompdf namespace
+use Dompdf\Dompdf;
+
+// instantiate and use the dompdf class
+$dompdf = new Dompdf();
+
+$dompdf->loadHtml('<h1>Welcome to CodexWorld.com</h1>');
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'landscape');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF to Browser
+//$dompdf->stream();
+
+// Output the generated PDF (1 = download and 0 = preview)
+$dompdf->stream("codex",array("Attachment"=>0));
+?>
