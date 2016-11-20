c23c160b13f0114a18e326389d9028fcb2583b6f
Delushaan <delushaan@outlook.com>
2016-11-20 15:27:21 +0530

    php_force_download_file

---
+++ php_force_download_file/download.php
@@ -0,0 +1,19 @@
+<?php
+if(!empty($_GET['file'])){
+    $fileName = basename($_GET['file']);
+    $filePath = 'files/'.$fileName;
+    if(!empty($fileName) && file_exists($filePath)){
+        // Define headers
+        header("Cache-Control: public");
+        header("Content-Description: File Transfer");
+        header("Content-Disposition: attachment; filename=$fileName");
+        header("Content-Type: application/zip");
+        header("Content-Transfer-Encoding: binary");
+        
+        // Read the file
+        readfile($filePath);
+        exit;
+    }else{
+        echo 'The file does not exist.';
+    }
+}
